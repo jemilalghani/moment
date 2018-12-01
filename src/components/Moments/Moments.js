c6de5100d74974d87be941f24034dcd3127dca23
Thomas <thomas@Thomass-MacBook-Air.local>
2018-11-30 19:12:53 -0700

    added registration/state

---
+++ src/components/Moments/Moments.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Moments.scss';
+
+class Moments extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Moments;

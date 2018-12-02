2d01d076e1fd2d37117e2b9d2307cc0197d0f58e
Jemila Al-Ghani <jalghani@Jemilas-MacBook-Pro.local>
2018-12-02 15:19:20 -0700

    Fix Controller Errors

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const massive = require('massive');
 
 // controllers 
 const bcryptController = require('./controllers/bcryptController');
+const momentController = require('./controllers/momentController');
 
 require('dotenv').config();
 
@@ -29,18 +30,18 @@ app.post('/api/logout', bcryptController.logout);
 // ADMIN POST FOR EXERIENCE DUMMY DATA
 app.post('/api/moment/admin', momentController.addDummy); // FRONTEND READY
 // EXPERIENCES (MOMENTS) TABLE
-app.post('/api/moment/create', momentController.addMoment);
-app.get('/api/moment/hostFind', momentController.findByHost)
-app.get('/api/moment/:id', momentController.findId);
-app.get('/api/moment/:highlight', momentController.findHighlight);
-app.get('/api/moment/:locale', momentController.findLocale);
+// app.post('/api/moment/create', momentController.addMoment);
+// app.get('/api/moment/hostFind', momentController.findByHost)
+// app.get('/api/moment/:id', momentController.findId);
+// app.get('/api/moment/:highlight', momentController.findHighlight);
+// app.get('/api/moment/:locale', momentController.findLocale);
 // ORDERS TABLE
-app.get('/api/order/hostFind')
-app.post('/api/order/userCreate')
-app.get('/api/order/userFind')
+// app.get('/api/order/hostFind')
+// app.post('/api/order/userCreate')
+// app.get('/api/order/userFind')
 // REVIEWS TABLE
-app.post('/api/review/create')
-app.get('/api/review/find')
+// app.post('/api/review/create')
+// app.get('/api/review/find')
 
 const SERVER_PORT = 4000;
 app.listen(SERVER_PORT, ()=>{
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {Switch, Route } from 'react-router-dom';
 import DetailedMoments from './components/DetailedMoments/DetailedMoments';
 import LoginRegister from './components/LoginRegister/LoginRegister';
-import Moments from './components/Moments/Moments'
+import Moments from './components/Moments/Moments';
+import Admin from './components/Admin/Admin';
 
 export default (
     <Switch>
-        {/* <Route path='/admin' component={}/> */}
+        <Route path='/admin' component={Admin}/>
         {/* <Route path='/checkout' component ={} /> */}
         {/* <Route path='/host' component ={} /> */}
         {/* <Route path='/user' component ={} /> */}

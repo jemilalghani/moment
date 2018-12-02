77da545e6d7f7f1b9336a3236b032d5b7c2e4749
Matt Shields <matthew.duncan.shields@gmail.com>
2018-12-01 17:23:49 -0700

    find_user added, updates to other sql files

--- db/create_user.sql
+++ db/create_user.sql
@@ -1,4 +1,4 @@
 INSERT INTO profiles
 (user_name, password, name_first, name_last, gender, email, phone, locale, about, prof_photo_url)
 VALUES
-($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
+($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;

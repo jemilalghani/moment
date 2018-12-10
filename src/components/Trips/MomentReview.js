39aeb6e2139e4f32ba6702176363184a8a447c2a
Matt Shields <matthew.duncan.shields@gmail.com>
2018-12-10 13:22:58 -0700

    working on reviews

---
+++ src/components/Trips/MomentReview.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import starFull from "../../Image/moment-star-full.svg";
+import starEmpty from "../../Image/moment-star-empty.svg";
+
+export default class MomentReview extends Component {
+  constructor() {
+    super();
+    this.state = {
+      stars: 3,
+      starsMax: [1, 2, 3, 4, 5]
+    };
+  }
+  changeStars = e => {
+    console.log("e", e.target);
+    this.setState({ stars: e.target.value });
+  };
+
+  render() {
+    const { stars, starsMax } = this.state;
+    const starMap = starsMax.map(star => {
+      if (stars >= star) {
+        return (
+          <div value={star}>
+            <img className="star" onClick={this.changeStars} src={starFull} />{" "}
+          </div>
+        );
+      } else {
+        return (
+          <img className="star" onClick={this.changeStars} src={starEmpty} />
+        );
+      }
+    });
+
+    return (
+      <>
+        {this.props.review === true ? (
+          <div className="moment-card-wide-grey">
+            <div className="moment-review">
+              <h3>Review Title</h3>
+              <div className="review-desc">Give your review a title.</div>
+              <textarea type="text" placeholder="Title" />
+              <h3>Review Description</h3>
+              <div className="review-desc">
+                Decribe your experience, keeping in mind the expectations the
+                host set with their experience description. Include any high or
+                low points that affected your rating.
+              </div>
+              <textarea type="text" placeholder="Description" />
+              <h3>Star Rating</h3>
+              <div className="review-desc">
+                Give a rating in stars. 1 being a low score and 5 being high
+                score.
+              </div>
+              {/* <textarea type="text" placeholder="Stars given out of five" /> */}
+              {starMap}
+              <button className="mcw-nextbutton-on">Submit</button>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+      </>
+    );
+  }
+}
